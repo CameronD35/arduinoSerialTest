53bc471517da6bc553051341d65e1869042309d1
CameronD35 <camster09@ca.rr.com>
2024-01-16 14:36:36 -0800

    Added file for database transmission (WIP) and .gitignore.

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
+from flask import Flask
+from database import db, connect_db, Temp
 import serial
 import time
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:3416SbSp13MS@localhost/arduino"
+app.config["SQLALCHEMY_ECHO"] = True
 
-ser = serial.Serial('COM3', 9600, timeout=1)
+connect_db(app)
 
-print("Reading data from serial port...")
-time.sleep(2)
-ser.reset_input_buffer()
+with app.app_context():
+    db.create_all()
 
-for i in range (0, 7):
-    line = ser.readline()
-    print(line)
-    time.sleep(0.5)
+
+# ser = serial.Serial('COM3', 9600, timeout=1)
+
+# print("Reading data from serial port...")
+# time.sleep(2)
+# ser.reset_input_buffer()
+
+# for i in range (0, 7):
+#     line = ser.readline()
+#     print(line.toString())
+#     time.sleep(0.5)
+
+#     temp = Temp(temperature=line)
+
+#     db.session.add(temp)
+#     db.session.commit
 
 print("Exit")
---
+++ database.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Temp(db.Model):
+
+    __tablename__ = "temps"
+
+    id = db.Column(db.Integer, primary_key=True)
+    temperature = db.Column(db.Text)
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
