cec2996a26e9044cf4c5f2818278692ac923ea1e
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-01-16 13:43:17 -0800

    Initialize files

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-# Test
+import serial
+import time
+
+
+ser = serial.Serial('/dev/cu.usbmodem1101', 9600, timeout=1)
+
+print("Reading data from serial port...")
+time.sleep(2)
+ser.reset_input_buffer()
+
+for i in range (0, 7):
+    line = ser.readline()
+    print(line)
+    time.sleep(0.5)
+
+print("Exit")
